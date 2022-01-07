edaa15fd0108bae6db15fa4bfd3233889f113407
Angelo Gino Varrati <angelo.varrati@outlook.it>
2022-01-07 21:43:50 +0100

    remove sp data from azure rm provider config

--- iac/modules/azure_cloud/provider.tf
+++ iac/modules/azure_cloud/provider.tf
@@ -2,17 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.89.0"
+      version = "2.89.0"
     }
   }
 }
 
 provider "azurerm" {
-  client_id       = var.sp_client_id
-  client_secret   = var.sp_client_secret
   subscription_id = var.az_sub_id
   tenant_id       = var.tenant_id
 
   features {
+
   }
 }
