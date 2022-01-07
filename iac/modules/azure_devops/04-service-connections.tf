a09e0d9fdf23995ffbd5879679a743330f3310cc
Angelo Gino Varrati <angelo.varrati@outlook.it>
2022-01-07 21:44:34 +0100

    add azure rm and docker service connection

--- iac/modules/azure_devops/04-service-connections.tf
+++ iac/modules/azure_devops/04-service-connections.tf
@@ -12,3 +12,14 @@ resource "azuredevops_serviceendpoint_azurerm" "azure_sc" {
   azurerm_subscription_id   = var.az_sub_id
   azurerm_subscription_name = var.az_sub_name
 }
+
+resource "azuredevops_serviceendpoint_dockerregistry" "acr_sc" {
+  project_id            = azuredevops_project.public.id
+  service_endpoint_name = "ACR"
+  description           = "Service connection to Azure Container Registry - @AngelusGi - #XmasDev2021"
+
+  docker_registry = var.az_acr_url
+  docker_username = var.az_acr_user
+  docker_password = var.az_acr_pwd
+  registry_type   = "Others"
+}
