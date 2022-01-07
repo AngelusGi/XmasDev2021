6b41ac33455dcfdb45a919ae65f518af39b6353d
Angelo Gino Varrati <angelo.varrati@outlook.it>
2022-01-07 21:41:22 +0100

    rbac on rg and reader on subscription

--- iac/modules/azure_cloud/03-rbac.tf
+++ iac/modules/azure_cloud/03-rbac.tf
@@ -1,5 +1,11 @@
-resource "azurerm_role_assignment" "rbac" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Contributor"
-  principal_id         = var.sp_obj_id
+resource "azurerm_role_assignment" "rg_rbac" {
+  scope              = azurerm_resource_group.rg.id
+  role_definition_id = data.azurerm_role_definition.builtin_rbac.id
+  principal_id       = data.azuread_service_principal.sp_devops.object_id
+}
+
+resource "azurerm_role_assignment" "sub_reader" {
+  scope              = data.azurerm_subscription.subscription_scope.id
+  role_definition_id = data.azurerm_role_definition.reader.id
+  principal_id       = data.azuread_service_principal.sp_devops.object_id
 }
